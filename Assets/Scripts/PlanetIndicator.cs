1dd5aa8f144ce887cedc231bd97f64555696f9e3
nomes <noahbweiner@gmail.com>
2021-06-18 09:29:35 -0700

    wire up distance label on planet indicator

--- Assets/Scripts/PlanetIndicator.cs
+++ Assets/Scripts/PlanetIndicator.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PixelText;
 using UnityEngine;
 
 public class PlanetIndicator : MonoBehaviour
 {
     public GameObject planet;
+    public GameObject spaceship;
+
+    public PixelText nameLabel;
+    public PixelText distanceLabel;
+    public PixelText typeLabel;
 
     private RectTransform rectTransform;
 
@@ -21,5 +27,8 @@ public class PlanetIndicator : MonoBehaviour
         }
 
         rectTransform.anchoredPosition = Camera.main.WorldToScreenPoint(planet.transform.position);
+
+        float distance = (spaceship.transform.position - planet.transform.position).magnitude;
+        distanceLabel.text = $"{distance.ToString("F1")} KM";
     }
 }
